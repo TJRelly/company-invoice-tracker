44a0f1810778c51cf0390a5e08f6e9ca9ad71a6c
tjrelly <xxterrelljacksonxx@gmail.com>
2024-07-18 08:30:03 -0400

    added companies routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 
 const app = express();
-const ExpressError = require("./expressError")
+const ExpressError = require("./expressError");
 
 app.use(express.json());
 
@@ -12,21 +12,17 @@ app.use("/companies", uRoutes);
 
 /** 404 handler */
 
-app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+app.use(function (req, res, next) {
+    const err = new ExpressError("Not Found", 404);
+    return next(err);
 });
 
 /** general error handler */
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-
-  return res.json({
-    error: err,
-    message: err.message
-  });
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ error: { message, status } });
 });
-
 
 module.exports = app;
